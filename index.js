16888192fd548d687a82f69477d3176465ccc31c
ZRD <ZRD@DESKTOP-2S08M61>
2023-06-30 12:28:32 +0200

    Added LoggedInElsewhere handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const SteamUsr = require('steam-user');
 const acc1 = new SteamUsr();
 const cfg = require('./config.json');
 acc1.logOn(cfg);
+
 acc1.on('loggedOn',()=>{
     console.log("Succesfully logged to: " + cfg.accountName);
     if(cfg.visible == true)
@@ -13,4 +14,10 @@ acc1.on('loggedOn',()=>{
     }
     acc1.gamesPlayed(cfg.acc1games);
     console.log("Farming hours on:" + cfg.acc1games);
+})
+
+acc1.on('LoggedInElsewhere',()=>{
+    console.log("Someone logged into account");
+    console.log("trying to relogin");
+    acc1.logOn(cfg);
 })
